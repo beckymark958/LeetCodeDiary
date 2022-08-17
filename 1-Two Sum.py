709a1e48584cf83aef067ee5f86c139f081f65d2
Becky Yu <beckymark958@gmail.com>
2022-08-16 21:27:05 -0400

    #1

---
+++ 1-Two Sum.py
@@ -0,0 +1,26 @@
+#Runtime: 108 ms, faster than 55.74% of Python3 online submissions for Two Sum.
+#Memory Usage: 15.1 MB, less than 64.41% of Python3 online submissions for Two Sum.
+
+# -- Solution 1 --
+# Time: O(n^2)
+# Space: O(1)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+# -- Solution 2 --
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mapping = {}
+        for i in range(len(nums)):
+            compliment = target - nums[i]
+            if compliment in mapping:
+                return [i, mapping[compliment]]
+            mapping[nums[i]] = i
+
+            
